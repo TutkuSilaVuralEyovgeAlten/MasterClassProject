f301bf71dab3a0c5019af7c0b8f70478f7b78993
TutkuSilaVuralEyovgeAlten <tutku.sila.vural.eyovge@alten.nl>
2022-10-03 22:01:01 +0200

    Add project files.

---
+++ MasterClassProject/Temperature.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    double temp;
+    cout << "What is the temperature?" << endl;
+    cin >> temp;
+
+    double fah = (1.8 * temp) + 32;
+
+    cout << "Temperature is " << fah << " Fahrenheit" << endl;
+
+    return 0;
+}
